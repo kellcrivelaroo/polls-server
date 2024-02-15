c309c09c34e4d46935de93ac74b7fa6afc45fe49
kell <kellcrivelaro92@gmail.com>
2024-02-15 08:57:17 -0300

    vercel config

---
+++ api/serverless.js
@@ -0,0 +1,16 @@
+"use strict";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+import Fastify from "fastify";
+
+const app = Fastify({
+  logger: true,
+});
+
+app.register(import("../src/http/server"));
+
+export default async (req, res) => {
+  await app.ready();
+  app.server.emit('request', req, res);
+}
